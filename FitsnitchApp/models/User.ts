aad3197e27223654b6e9ab87d0ad2c4b10396f80
rcaina <renzini2018@gmail.com>
2021-11-16 23:52:54 -0700

    better use of code, layout general look of user profile page

--- FitsnitchApp/models/User.ts
+++ FitsnitchApp/models/User.ts
@@ -2,10 +2,12 @@ export default class User {
     public id: string;
     public firstname: string;
     public lastname: string;
+    public updates: String[];
 
     constructor(id:string,firstname:string,lastname:string) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
+        this.updates = ["one", "two", "three"];
     }
 }
--- FitsnitchApp/views/UserProfileView.tsx
+++ FitsnitchApp/views/UserProfileView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, Image, Dimensions, useWindowDimensions } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, Dimensions, useWindowDimensions, ScrollView, FlatList, SectionList } from 'react-native';
 import Profile from '../components/Profile';
 import User from '../models/User';
 
@@ -10,108 +10,183 @@ const EMPTY_COLOR = "grey";
 const PROGRESS_COLOR = "green";
 const SIZE = 45;
 
-
 const UserProfileView: React.FC = () => {
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image
-            source={{
-              uri:
-                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',
-            }}
-            //borderRadius will help to make Round Shape
-            style={{
-              width: 75,
-              height: 75,
-              borderRadius: 200 / 2,
-            }}
-          />
-         <Text style={styles.headerText}>{dummyUser.firstname} {dummyUser.lastname}</Text>
-      </View>
+    <View style={[styles.container]}>
+      <ScrollView style={styles.scrollView}>
+        <View style={[styles.header]}>
+          <Image
+              source={{
+                uri:
+                  'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',
+              }}
+              //borderRadius will help to make Round Shape
+              style={{
+                width: 75,
+                height: 75,
+                borderRadius: 200 / 2,
+              }}
+            />
+          <Text style={styles.headerText}>{dummyUser.firstname} {dummyUser.lastname}</Text>
+        </View>
 
-      <View style={[styles.body, {justifyContent: 'space-around'}]}>
-        <Text>
-          Trainer: Add A Trainer
-        </Text>
-        <Text>
-          5 Partners: Chef Rush, +4
-        </Text>
-      <View style={[styles.rowContainer, {justifyContent: 'space-around'}]}>
-        <View style={{flex: 1}}>
-          <Text style={{fontSize: 17, fontWeight: 'bold'}}>
-              Your Goals
-           </Text>
-        </View>
-        <View style={{}}>
-          <Text>
-            Edit
-          </Text>
-        </View>
-      </View>
-        <View style={[styles.rowContainer, {backgroundColor: 'white'}]}>
-          <View style={styles.progressCircle}/>
-          <View style={styles.progressCircle}/>
-          <View style={styles.progressCircle}/>
-          <View style={styles.progressCircle}/>
-        </View>
-      </View>
-
-      <View style={[styles.body, {justifyContent: 'space-around'}]}>
-        <View style={styles.rowContainer}>
-          <View style={{flex: 1}}>
-            <Text style={{fontSize: 17, fontWeight: 'bold'}}>
-                Your Plan
-            </Text>
-          </View>
+        <ScrollView style={styles.scrollView}>
           <View style={{}}>
-            <Text>
-              Edit
-            </Text>
-          </View>
-        </View>
-        <View style={[styles.rowContainer, {backgroundColor: 'white'}]}>
-          <View style={styles.progressCircle}/>
-        </View>
-        <View style={[styles.rowContainer]}>
-          <View style={{flex: 1}}>
-            <Text style={{fontSize: 17, fontWeight: 'bold'}}>
-                Cheats Summary
-            </Text>
-          </View>
-          <View style={{}}>
-            <Text>
-              Edit
-            </Text>
-          </View>
-        </View>
-        <View style={[styles.rowContainer, {backgroundColor: 'white'}]}>
-          <View style={styles.progressCircle}/>
-        </View>
-      </View>
+            {/* <View style={[styles.body]}>
+              <Text>
+                Trainer: Add A Trainer
+              </Text>
+              <Text>
+                5 Partners: Chef Rush, +4
+              </Text>
+              <View style={[styles.rowContainer, {justifyContent: 'space-around'}]}>
+                <View style={{flex: 1}}>
+                  <Text style={{fontSize: 17, fontWeight: 'bold'}}>
+                      Your Goals
+                  </Text>
+                </View>
+                <View style={{}}>
+                  <Text>
+                    Edit
+                  </Text>
+                </View>
+              </View>
+              <View style={[styles.rowContainer, {backgroundColor: 'white', padding: 10}]}>
+                <View style={styles.progressCircle}/>
+                <View style={styles.progressCircle}/>
+                <View style={styles.progressCircle}/>
+                <View style={styles.progressCircle}/>
+              </View>
+            </View> */}
 
-      <View style={[styles.body, {justifyContent: 'space-around'}]}>
-        <View style={styles.rowContainer}>
-          <View style={{flex: 1}}>
-            <Text style={{fontSize: 17, fontWeight: 'bold'}}>
-                Your Updates
-            </Text>
-          </View>
-          <View style={{}}>
-            <Text>
-              Edit
-            </Text>
-          </View>
-        </View>
-        <View style={[styles.rowContainer, {backgroundColor: 'white'}]}>
-          <View style={styles.progressCircle}/>
-        </View>
-        <View style={[styles.rowContainer, {backgroundColor: 'white'}]}>
-          <View style={styles.progressCircle}/>
-        </View>
-      </View>
+            <View style={[styles.body]}>
+              <Text style={{fontSize: 15, paddingBottom: 5}}>
+                Trainer: Add A Trainer
+              </Text>
+              <Text style={{fontSize: 15, paddingBottom: 5}}>
+                5 Partners: Chef Rush, +4
+              </Text>
+              <View style={[styles.rowContainer, {justifyContent: 'space-around'}]}>
+                <View style={{flex: 1}}>
+                  <Text style={{fontSize: 17, fontWeight: 'bold'}}>
+                      Goals
+                  </Text>
+                </View>
+                <View style={{}}>
+                  <Text>
+                    Edit
+                  </Text>
+                </View>
+              </View>
+              <View style={[styles.rowContainer, {backgroundColor: 'white', padding: 10, backgroundColor: 'lightgrey'}]}>
+                <View style={styles.progressCircle}/>
+                <View style={styles.progressCircle}/>
+                <View style={styles.progressCircle}/>
+                <View style={styles.progressCircle}/>
+              </View>
 
+
+              <View style={styles.rowContainer}>
+                <View style={{flex: 1}}>
+                  <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
+                      Plan
+                  </Text>
+                </View>
+                <View style={{}}>
+                  <Text>
+                    Edit
+                  </Text>
+                </View>
+              </View>
+              <View style={[styles.rowContainer, {backgroundColor: 'lightgrey'}]}>
+                <Text style={{fontSize: 15, padding: 20}}>Update your Plan</Text>
+              </View>
+              <View style={[styles.rowContainer]}>
+                <View style={{flex: 1}}>
+                  <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
+                      Cheats Summary
+                  </Text>
+                </View>
+                <View style={{}}>
+                  <Text>
+                    Edit
+                  </Text>
+                </View>
+              </View>
+              <View style={[styles.rowContainer, {backgroundColor: 'lightgrey'}]}>
+              <Text style={{fontSize: 15, padding: 20}}>No Cheats to report</Text>
+              </View>
+            {/* </View> */}
+
+            {/* <View style={[styles.body]}> */}
+              <View style={styles.updateHeader}>
+                <View style={{flex: 1}}>
+                  <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
+                      Updates
+                  </Text>
+                </View>
+                <View style={{}}>
+                  <Text>
+                    Edit
+                  </Text>
+                </View>
+              </View>
+              {/* <SectionList style={styles.updates}
+          sections={[
+            {data: dummyUser.updates},
+          ]}
+          renderItem={({item}) => <Text style={styles.updates}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.updates}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}
+        /> */}
+              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+                <Text>
+                  Update 1
+                </Text>
+              </View>
+              <Text></Text>
+              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+                <Text>
+                  Update 2
+                </Text>
+              </View>
+              <Text></Text>
+              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+                <Text>
+                  Update 3
+                </Text>
+              </View>
+              <Text></Text>
+              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+                <Text>
+                  Update 4
+                </Text>
+              </View>
+              <Text></Text>
+              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+                <Text>
+                  Update 5
+                </Text>
+              </View>
+              <Text></Text>
+              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+                <Text>
+                  Update 6
+                </Text>
+              </View>
+              <Text></Text>
+              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+                <Text>
+                  Update 7
+                </Text>
+              </View>
+              <Text></Text>
+            {/* </View> */}
+            </View>
+          </View>
+        </ScrollView>
+      </ScrollView>
     </View>
   );
 };
@@ -119,32 +194,37 @@ const UserProfileView: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
   },
   greeting: {
     fontSize: 0,
     fontWeight: 'bold',
-    margin: 16
+    margin: 16,
   },
   rowContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+  },
+  updateHeader: {
+    flexDirection: 'row',
   },
   header: {
     flex: 1,
+    maxHeight:150,
+    position: 'relative',
     flexDirection: 'row',
     backgroundColor: 'white',
     padding: 20,
     textAlign: 'left',
     alignItems: 'center',
     borderBottomColor: 'black',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: 2,
+    borderTopColor: 'black',
+    borderTopWidth: StyleSheet.hairlineWidth,
   },
   headerText: {
     flex: 2,
     backgroundColor:'white', 
     color: 'black',
-    alignSelf: 'center',
     textAlign: 'center',
     fontSize: 40,
   },
@@ -153,13 +233,15 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   },
   body: {
-    flex: 2,
-    backgroundColor: 'blue',
+    position: 'relative',
+    flex: 1,
+    backgroundColor: 'white',
     padding: 20,
+    justifyContent: 'space-around'
   },
   body2: {
     flex: 2,
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
     padding: 20,
   },
   progressCircle: {
@@ -182,7 +264,15 @@ const styles = StyleSheet.create({
     borderTopColor: PROGRESS_COLOR,
     borderRightColor: 'transparent',
     borderBottomColor: 'transparent',
-  }
+  },
+
+  updates: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    fontSize: 15, 
+    padding: 20
+  },
 });
 
 export default UserProfileView;
