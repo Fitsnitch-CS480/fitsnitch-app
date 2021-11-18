0a6b8cc724175ae36d414caa1fc7e632a3bb65fe
Filaipus <filipe_bilipe@hotmail.com>
2021-11-17 22:47:39 -0700

    Created LoginView and SignUp View Created Navigators for the whole application. Now, if the user is logged in it goes directly into the main application. Otherwise, it goes into a Login/Signup screen

--- FitsnitchApp/App.tsx
+++ FitsnitchApp/App.tsx
@@ -1,72 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Button, Settings, StyleSheet, Text, View } from 'react-native';
+import LoginView from './views/LoginView';
 import SnitchesView from './views/SnitchesView';
 import UserProfileView from './views/UserProfileView';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import PeopleView from './views/PeopleView';
 import SettingsView from './views/SettingsView';
+import MainNavigator from './navigation/mainNavigator';
 
-const Hello: React.FC = () => {
-  const Tab = createBottomTabNavigator();
-
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-      initialRouteName="Profile"
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName = "";
-
-            switch (route.name) {
-              case "Snitches":
-                iconName = 'campaign';
-                break;
-                
-              case "Profile":
-                iconName = 'person';
-                break;
-
-              case "People":
-                iconName = 'people';
-                break;
-                
-              case "Settings":
-                iconName = 'settings';
-                break;
-            
-              default:
-                break;
-            }
-
-            return <Icon name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Snitches" component={SnitchesView} />
-        <Tab.Screen name="Profile" component={UserProfileView} />
-        <Tab.Screen name="People" component={PeopleView} />
-        <Tab.Screen name="Settings" component={SettingsView} />
-      </Tab.Navigator>
-    </NavigationContainer>
-
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  greeting: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 16
-  }
-});
-
-export default Hello;
+export default function App(){
+  return(
+    <MainNavigator/>
+  )
+}
---
+++ FitsnitchApp/navigation/loginNavigator.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginView from "../views/LoginView";
+import SignUpView from "../views/SignUpView";
+import { NavigationContainer } from '@react-navigation/native';
+
+const stackNavigator = createNativeStackNavigator();
+
+const LoginNavigator : React.FC = () => {
+    return(
+        
+        <stackNavigator.Navigator>
+            <stackNavigator.Screen name="login" component={LoginView} options={{headerShown: false,}}/>
+            <stackNavigator.Screen name="signup" component={SignUpView} options={{headerShown: false,}}/>            
+        </stackNavigator.Navigator>
+        
+        
+    )
+}
+
+export default LoginNavigator;
