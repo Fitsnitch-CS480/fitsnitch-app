4c85f7f4c59c6ca886932aeb4b559d0d6c00d7b1
rcaina <renzini2018@gmail.com>
2021-11-22 10:55:51 -0700

    swtich userProfile view code to be in Profile component

--- FitsnitchApp/views/UserProfileView.tsx
+++ FitsnitchApp/views/UserProfileView.tsx
@@ -13,163 +13,166 @@ const SIZE = 45;
 const UserProfileView: React.FC = () => {
 
   return (
-    <View style={[styles.container]}>
-      <ScrollView style={styles.scrollView}>
-        <View style={[styles.header]}>
-          <Image
-              source={{
-                uri:
-                  'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',
-              }}
-              //borderRadius will help to make Round Shape
-              style={{
-                width: 75,
-                height: 75,
-                borderRadius: 200 / 2,
-              }}
-            />
-          <Text style={styles.headerText}>{dummyUser.firstname} {dummyUser.lastname}</Text>
-        </View>
+    <Profile user={dummyUser} />
+    // <View style={[styles.container]}>
+    //   <ScrollView style={styles.scrollView}>
+    //     <View style={[styles.header]}>
+    //       <Image
+    //           source={{
+    //             uri:
+    //               'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',
+    //           }}
+    //           //borderRadius will help to make Round Shape
+    //           style={{
+    //             width: 75,
+    //             height: 75,
+    //             borderRadius: 200 / 2,
+    //           }}
+    //         />
+    //       <Text style={styles.headerText}>{dummyUser.firstname} {dummyUser.lastname}</Text>
+    //     </View>
 
-        <ScrollView style={styles.scrollView}>
-          <View style={{}}>
-            <View style={[styles.body]}>
-              <Text style={{fontSize: 15, paddingBottom: 5}}>
-                Trainer: Add A Trainer
-              </Text>
-              <Text style={{fontSize: 15, paddingBottom: 5}}>
-                5 Partners: Chef Rush, +4
-              </Text>
-              <View style={[styles.rowContainer, {justifyContent: 'space-around'}]}>
-                <View style={{flex: 1}}>
-                  <Text style={{fontSize: 17, fontWeight: 'bold'}}>
-                      Goals
-                  </Text>
-                </View>
-                <View style={{}}>
-                  <Text>
-                    Edit
-                  </Text>
-                </View>
-              </View>
-              <View style={[styles.rowContainer, {padding: 10, backgroundColor: 'lightgrey'}]}>
-                <View style={styles.progressCircle}/>
-                <View style={styles.progressCircle}/>
-                <View style={styles.progressCircle}/>
-                <View style={styles.progressCircle}/>
-              </View>
-
-
-              <View style={styles.rowContainer}>
-                <View style={{flex: 1}}>
-                  <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
-                      Plan
-                  </Text>
-                </View>
-                <View style={{}}>
-                  <Text>
-                    Edit
-                  </Text>
-                </View>
-              </View>
-              <View style={[styles.rowContainer, {backgroundColor: 'lightgrey'}]}>
-                <Text style={{fontSize: 15, padding: 20}}>Update your Plan</Text>
-              </View>
-              <View style={[styles.rowContainer]}>
-                <View style={{flex: 1}}>
-                  <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
-                      Cheats Summary
-                  </Text>
-                </View>
-                <View style={{}}>
-                  <Text>
-                    Edit
-                  </Text>
-                </View>
-              </View>
-              <View style={[styles.rowContainer, {backgroundColor: 'lightgrey'}]}>
-              <Text style={{fontSize: 15, padding: 20}}>No Cheats to report</Text>
-              </View>
+    //     <ScrollView style={styles.scrollView}>
+    //       <View style={{}}>
+    //         <View style={[styles.body]}>
+    //           <Text style={{fontSize: 15, paddingBottom: 5}}>
+    //             Trainer: Add A Trainer
+    //           </Text>
+    //           <Text style={{fontSize: 15, paddingBottom: 5}}>
+    //             5 Partners: Chef Rush, +4
+    //           </Text>
+    //           <View style={[styles.rowContainer, {justifyContent: 'space-around'}]}>
+    //             <View style={{flex: 1}}>
+    //               <Text style={{fontSize: 17, fontWeight: 'bold'}}>
+    //                   Goals
+    //               </Text>
+    //             </View>
+    //             <View style={{}}>
+    //               <Text>
+    //                 Edit
+    //               </Text>
+    //             </View>
+    //           </View>
+    //           <View style={[styles.rowContainer, {padding: 10, backgroundColor: 'lightgrey'}]}>
+    //             <View style={styles.progressCircle}/>
+    //             <View style={styles.progressCircle}/>
+    //             <View style={styles.progressCircle}/>
+    //             <View style={styles.progressCircle}/>
+    //           </View>
 
 
-              <View style={styles.updateHeader}>
-                <View style={{flex: 1}}>
-                  <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
-                      Updates
-                  </Text>
-                </View>
-                <View style={{}}>
-                  <Text>
-                    Edit
-                  </Text>
-                </View>
-              </View>
-              {/* <SectionList style={styles.updates}
-          sections={[
-            {data: dummyUser.updates},
-          ]}
-          renderItem={({item}) => <Text style={styles.updates}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.updates}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
-        /> */}
-              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
-                <Text>
-                  Update 1
-                </Text>
-              </View>
-              <Text></Text>
-              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
-                <Text>
-                  Update 2
-                </Text>
-              </View>
-              <Text></Text>
-              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
-                <Text>
-                  Update 3
-                </Text>
-              </View>
-              <Text></Text>
-              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
-                <Text>
-                  Update 4
-                </Text>
-              </View>
-              <Text></Text>
-              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
-                <Text>
-                  Update 5
-                </Text>
-              </View>
-              <Text></Text>
-              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
-                <Text>
-                  Update 6
-                </Text>
-              </View>
-              <Text></Text>
-              <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
-                <Text>
-                  Update 7
-                </Text>
-              </View>
-              <Text></Text>
-            </View>
-          </View>
-        </ScrollView>
-      </ScrollView>
-    </View>
+    //           <View style={styles.rowContainer}>
+    //             <View style={{flex: 1}}>
+    //               <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
+    //                   Plan
+    //               </Text>
+    //             </View>
+    //             <View style={{}}>
+    //               <Text>
+    //                 Edit
+    //               </Text>
+    //             </View>
+    //           </View>
+    //           <View style={[styles.rowContainer, {backgroundColor: 'lightgrey'}]}>
+    //             <Text style={{fontSize: 15, padding: 20}}>Update your Plan</Text>
+    //           </View>
+    //           <View style={[styles.rowContainer]}>
+    //             <View style={{flex: 1}}>
+    //               <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
+    //                   Cheats Summary
+    //               </Text>
+    //             </View>
+    //             <View style={{}}>
+    //               <Text>
+    //                 Edit
+    //               </Text>
+    //             </View>
+    //           </View>
+    //           <View style={[styles.rowContainer, {backgroundColor: 'lightgrey'}]}>
+    //           <Text style={{fontSize: 15, padding: 20}}>No Cheats to report</Text>
+    //           </View>
+
+
+    //           <View style={styles.updateHeader}>
+    //             <View style={{flex: 1}}>
+    //               <Text style={{fontSize: 17, fontWeight: 'bold', paddingTop: 10}}>
+    //                   Updates
+    //               </Text>
+    //             </View>
+    //             <View style={{}}>
+    //               <Text>
+    //                 Edit
+    //               </Text>
+    //             </View>
+    //           </View>
+    //           {/* <SectionList style={styles.updates}
+    //       sections={[
+    //         {data: dummyUser.updates},
+    //       ]}
+    //       renderItem={({item}) => <Text style={styles.updates}>{item}</Text>}
+    //       renderSectionHeader={({section}) => <Text style={styles.updates}>{section.title}</Text>}
+    //       keyExtractor={(item, index) => index}
+    //     /> */}
+    //           <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+    //             <Text>
+    //               Update 1
+    //             </Text>
+    //           </View>
+    //           <Text></Text>
+    //           <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+    //             <Text>
+    //               Update 2
+    //             </Text>
+    //           </View>
+    //           <Text></Text>
+    //           <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+    //             <Text>
+    //               Update 3
+    //             </Text>
+    //           </View>
+    //           <Text></Text>
+    //           <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+    //             <Text>
+    //               Update 4
+    //             </Text>
+    //           </View>
+    //           <Text></Text>
+    //           <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+    //             <Text>
+    //               Update 5
+    //             </Text>
+    //           </View>
+    //           <Text></Text>
+    //           <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+    //             <Text>
+    //               Update 6
+    //             </Text>
+    //           </View>
+    //           <Text></Text>
+    //           <View style={[styles.updates, {backgroundColor: 'lightgrey'}]}>
+    //             <Text>
+    //               Update 7
+    //             </Text>
+    //           </View>
+    //           <Text></Text>
+    //         </View>
+    //       </View>
+    //     </ScrollView>
+    //   </ScrollView>
+    // </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   greeting: {
-    fontSize: 0,
+    fontSize: 20,
     fontWeight: 'bold',
-    margin: 16,
+    margin: 16
   },
   rowContainer: {
     flexDirection: 'row',
