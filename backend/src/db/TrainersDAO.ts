fdca7f13fc039ce268b321e039e0321db934302e
Arthur Judd <amjudd315@gmail.com>
2022-01-12 14:28:30 -0700

    improved user and trainer daos

--- backend/src/db/TrainersDAO.ts
+++ backend/src/db/TrainersDAO.ts
@@ -1,8 +1,9 @@
 import TrainerClientPair from "../../../shared/models/TrainerClientPair";
 
-export default interface TrainersDAO {
-    assignTrainerToClient(trainerId:string,clientId:string);
-    isTrainerOfClient(trainerId:string,clientId:string): Promise<boolean>;
+export default interface TrainersDao {
+    assignTrainerToClient(data:TrainerClientPair);
+    isTrainerOfClient(data:TrainerClientPair): Promise<boolean>;
     getClientIdsOfTrainer(trainerId:string): Promise<string[]>;
     getTrainerIdOfClient(clientId:string): Promise<string>;
+    removeTrainerFromClient(data:TrainerClientPair);
 }
