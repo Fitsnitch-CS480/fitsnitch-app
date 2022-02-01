82207c58799171ad26c826a62a1c75e9079e383f
Arthur Judd <amjudd315@gmail.com>
2022-02-01 13:44:43 -0700

    differentiated snitch service and dao

--- backend/src/services/SnitchService.ts
+++ backend/src/services/SnitchService.ts
@@ -3,8 +3,13 @@ import {UserSnitchesRequest, UserSnitchesResponse} from "../../../react-native-a
 import {GetSnitchRequest} from "../../../react-native-app/shared/models/requests/GetSnitchRequest";
 import {CreateSnitchRequest} from "../../../react-native-app/shared/models/requests/CreateSnitchRequest";
 
-export default interface SnitchDao {
-    saveSnitch(data:SnitchEvent);
+export default interface SnitchService {
+    /**
+     * Creates a new Snitch. Handles saving the snitches datetime
+     * so that it can be mroe reliable than a users phone
+     * @param data Just the data required to create a new Snitch
+     */
+    createSnitch(data:CreateSnitchRequest): Promise<SnitchEvent>;
     updateSnitch(data: SnitchEvent);
     getSnitch(data:GetSnitchRequest): Promise<SnitchEvent|undefined>;
     getSnitchesForUsers(request:UserSnitchesRequest): Promise<UserSnitchesResponse>;
