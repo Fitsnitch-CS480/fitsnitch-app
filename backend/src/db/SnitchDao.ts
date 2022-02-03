c7d2fb623ca17dbe0d3f0cc5ee4ce85974b09920
Arthur Judd <amjudd315@gmail.com>
2022-02-03 10:22:32 -0700

    created snitch dao

--- backend/src/db/SnitchDao.ts
+++ backend/src/db/SnitchDao.ts
@@ -1,12 +1,12 @@
 import SnitchEvent from "../../../react-native-app/shared/models/SnitchEvent";
-import {UserSnitchesRequest, UserSnitchesResponse} from "../../../react-native-app/shared/models/requests/GetSnitchesRequest";
+import {UserSnitchesRequest, UserSnitchesResponse} from "../../../react-native-app/shared/models/requests/UserSnitchesRequest";
 import {GetSnitchRequest} from "../../../react-native-app/shared/models/requests/GetSnitchRequest";
 import {CreateSnitchRequest} from "../../../react-native-app/shared/models/requests/CreateSnitchRequest";
 
 export default interface SnitchDao {
-    saveSnitch(data:SnitchEvent);
+    createSnitch(data:SnitchEvent);
     updateSnitch(data: SnitchEvent);
-    getSnitch(data:GetSnitchRequest): Promise<SnitchEvent|undefined>;
+    getSnitch(data:GetSnitchRequest): Promise<SnitchEvent|null>;
     getSnitchesForUsers(request:UserSnitchesRequest): Promise<UserSnitchesResponse>;
     deleteSnitch(data:SnitchEvent);
 }
