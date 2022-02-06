0138984ebff0730e3fbba4fbaf71171d236faaef
Filaipus <filipe_bilipe@hotmail.com>
2022-02-05 23:42:56 -0700

    WIP changing SignUpView style (which will later affect LoginView). Instead of hardcodded mess, now we have flexible beauty.

--- react-native-app/views/SignUpView.tsx
+++ react-native-app/views/SignUpView.tsx
@@ -12,6 +12,8 @@ const SignUpView : React.FC = () => {
   const [email, onChangeEmail] = useState('');
   const [password, onChangePassword] = useState('');
   const [phoneNumber, onChangePhoneNumber] = useState('');
+  const [firstName, onChangeFirstName] = useState('');
+  const [lastName, onChangeLastName] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   
@@ -36,7 +38,7 @@ const SignUpView : React.FC = () => {
           console.log('Return from signUp information: ', cognitoSignUp);
           
           // Create User in DynamoDB too
-          let user = new User(cognitoSignUp.userSub,cognitoSignUp.user.getUsername(),undefined,undefined,undefined, phoneNumber)
+          let user = new User(cognitoSignUp.userSub,cognitoSignUp.user.getUsername(),firstName,lastName,undefined, phoneNumber)
           await ServerFacade.createUser(user);
           
           //Move to confirmation screen, user should get code in email.
@@ -64,147 +66,137 @@ const SignUpView : React.FC = () => {
   return (
       <View style={styles.container}>
       
-      <View style={styles.materialButtonPrimary}>
-        <Button title="Sign Up" onPress={signUpFunction}></Button>
+      <View style={styles.image}>
+        <Image
+        source={require("../assets/images/image_bnui..png")}
+        resizeMode="contain"
+        style={styles.image}
+        ></Image>
       </View>
       
-      <Text style={styles.or2}>--------------- OR ---------------</Text>
-      <Text style={styles.loremIpsum} onPress={() => navigation.navigate('login')}>Already have an account? Log In</Text> 
-      <View style={styles.image2Row}>
-        <Image
-          source={require("../assets/images/image_ia6Y..png")}
-          resizeMode="contain"
-          style={styles.image2}
-        ></Image>
-        <Text style={styles.logInWithGoogle}>Sign up with Google</Text>
-      </View>
-      <View style={styles.image3Row}>
-        <Image
-          source={require("../assets/images/image_S68k..png")}
-          resizeMode="contain"
-          style={styles.image3}
-        ></Image>
-        <Text style={styles.logInWithFacebook}>Sign up with Facebook</Text>
-      </View>
-      <View style={styles.image4Row}>
-        <Image
-          source={require("../assets/images/image_nFko..png")}
-          resizeMode="contain"
-          style={styles.image4}
-        ></Image>
-        <Text style={styles.logInWithTwitter}>Sign up with Twitter</Text>
-      </View>
       <View style={styles.materialUnderlineTextboxStack}>
         <TextInput placeholder="Username" onChangeText={onChangeEmail}></TextInput>
         <TextInput placeholder="Password" secureTextEntry onChangeText={onChangePassword}></TextInput>
         <TextInput placeholder="Phone Number" keyboardType='numeric' onChangeText={onChangePhoneNumber}></TextInput>
+        <TextInput placeholder="First Name" onChangeText={onChangeFirstName}></TextInput>
+        <TextInput placeholder="Last Name" onChangeText={onChangeLastName}></TextInput>
       </View>
-      <Image
-        source={require("../assets/images/image_bnui..png")}
-        resizeMode="contain"
-        style={styles.image}
-      ></Image>
+
+      <View style={styles.materialButtonPrimary}>
+        <Button title="Sign Up" onPress={signUpFunction}></Button>
+      </View>
+      
+      <Text style={styles.defaultText}>--------------- OR ---------------</Text>
+
+      <View style={styles.signUpRows}>
+        <Image
+          source={require("../assets/images/image_ia6Y..png")}
+          resizeMode="contain"
+          style={styles.googleIcon}
+        ></Image>
+        <Text style={styles.signUpOAuthText}>Sign up with Google</Text>
+        {/* <Text style={styles.logInWithGoogle}>Sign up with Google</Text> */}
+        
+      </View>
+
+      <View style={styles.signUpRows}>
+        <Image
+          source={require("../assets/images/image_S68k..png")}
+          resizeMode="contain"
+          style={styles.facebookIcon}
+        ></Image>
+        {/* <Text style={styles.logInWithFacebook}>Sign up with Facebook</Text> */}
+        <Text style={styles.signUpOAuthText}>Sign up with Facebook</Text>
+      </View>
+
+      <View style={styles.signUpRows}>
+        <Image
+          source={require("../assets/images/image_nFko..png")}
+          resizeMode="contain"
+          style={styles.twitterIcon}
+        ></Image>
+        <Text style={styles.signUpOAuthText}>Sign up with Twitter</Text>
+        {/* <Text style={styles.logInWithTwitter}>Sign up with Twitter</Text> */}
+      </View>
+      
+      <Text style={styles.defaultText} onPress={() => navigation.navigate('login')}>Already have an account? Log In</Text> 
+      
+      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    flexDirection: 'column',
+    // justifyContent: 'center',
+    alignItems: 'center'
   },
   materialButtonPrimary: {
-    height: 36,
+    // height: 36,
     width: 289,
-    marginTop: 388,
-    marginLeft: 52
+    paddingTop: 10,
+    // marginTop: 388,
+    // marginLeft: 52
+    flex: 1
   },
-  or2: {
+  defaultText: {
     fontFamily: "roboto-regular",
     color: "#121212",
-    marginTop: 43,
-    marginLeft: 126
+    textAlign: 'center',
+    // marginTop: 43,
+    // marginLeft: 126
+    flex: 1
   },
-  loremIpsum: {
+  signUpOAuthText: {
     fontFamily: "roboto-regular",
     color: "#121212",
-    marginTop: 201,
-    marginLeft: 91
+    // marginTop: 43,
+    marginLeft: 6,
+    flex: 1
   },
-  image2: {
+  googleIcon: {
     height: 33,
     width: 33
   },
-  logInWithGoogle: {
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    marginLeft: 6,
-    marginTop: 8
+  signUpRows: {
+    // height: 33,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: 180
+    // alignItems: 'center',
+    
+    
+    // paddingLeft: 91,
+    
+    // marginTop: -170,
+    // marginLeft: 91,
+    // marginRight: 130
   },
-  image2Row: {
-    height: 33,
-    flexDirection: "row",
-    marginTop: -170,
-    marginLeft: 91,
-    marginRight: 130
-  },
-  image3: {
-    height: 27,
-    width: 27
-  },
-  logInWithFacebook: {
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    marginLeft: 9,
-    marginTop: 5
-  },
-  image3Row: {
-    height: 27,
-    flexDirection: "row",
-    marginTop: 1,
-    marginLeft: 94,
-    marginRight: 113
-  },
-  image4: {
+  facebookIcon: {
     height: 30,
     width: 30
   },
-  logInWithTwitter: {
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    marginLeft: 7,
-    marginTop: 6
-  },
-  image4Row: {
+  twitterIcon: {
     height: 30,
-    flexDirection: "row",
-    marginLeft: 93,
-    marginRight: 132
-  },
-  materialUnderlineTextbox: {
-    height: 43,
-    width: 289,
-    position: "absolute",
-    left: 0,
-    top: 0,
-    placeholder: "Sign up"
+    width: 30
   },
   materialUnderlineTextboxStack: {
     width: 289,
-    height: 43,
-    marginTop: -359,
-    marginLeft: 52
+    // height: 43,
+    flex: 5
+    // marginTop: -359,
+    // marginLeft: 52
   },
   image: {
     height: 200,
     width: 200,
-    marginTop: -243,
-    marginLeft: 79
-  },
-  materialUnderlineTextbox1: {
-    height: 43,
-    width: 289,
-    marginTop: 57,
-    marginLeft: 52
+    flex: 5,
+    // paddingTop: 50
+    // marginTop: -243,
+    // marginLeft: 79
   }
 });
 
