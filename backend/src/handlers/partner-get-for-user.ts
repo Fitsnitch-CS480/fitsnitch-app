48d5a347ad2435526f924566d4b194f756e05d8f
Arthur Judd <amjudd315@gmail.com>
2022-02-09 21:21:21 -0700

    made get-partners handler return users

--- backend/src/handlers/partner-get-for-user.ts
+++ backend/src/handlers/partner-get-for-user.ts
@@ -1,11 +1,12 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import LambaUtils from "../utils/LambdaUtils";
 import PartnerAssociationService from "../services/PartnerAssociationService";
+import User from "../../../react-native-app/shared/models/User";
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
-    return await LambaUtils.handleEventWithBody<string>(event, async (pair,res)=>{
-        let partnerId2:string[] = await new PartnerAssociationService().getPartnerIdsOfUser(pair);
-        res.setBodyToData(partnerId2);
+    return await LambaUtils.handleEventWithBody<string>(event, async (userId,res)=>{
+        let partners:User[] = await new PartnerAssociationService().getPartnersOfUser(userId);
+        res.setBodyToData(partners);
         res.setCode(200);
         return res;
     });
