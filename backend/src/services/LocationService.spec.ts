172bd506514764c02e17c4a3c440c24f38b09431
Arthur Judd <amjudd315@gmail.com>
2022-02-17 01:52:14 -0700

    determines inside of restaurant if boundary exists, else geometric closest

--- backend/src/services/LocationService.spec.ts
+++ backend/src/services/LocationService.spec.ts
@@ -1,16 +1,150 @@
-import { LatLongPair } from "../../../react-native-app/shared/models/CoordinateModels";
+import { LatLonPair } from "../../../react-native-app/shared/models/CoordinateModels";
 import { LocationService } from "./LocationService";
 
 describe(LocationService, ()=>{
     
     describe(LocationService.getRestaurantAtLocation, ()=>{
+        jest.setTimeout(10000)
+
         /** This is very much an integration test using actual APIS!!! */
-        it("should locate Domino's", async ()=>{
-            let location = new LatLongPair(40.2508,-111.6613);
+        it("should locate Domino's as only option", async ()=>{
+            let location = new LatLonPair(40.25085, -111.6612840);
             let res = await LocationService.getRestaurantAtLocation(location);
             // console.log(res);
             expect(res).toBeDefined();
             expect(res?.name).toBe("Domino's");
         })
+
+        it("should locate McDs from far drive-thru", async ()=>{
+            /*
+            ---------
+            |       |
+            |       |
+            | McD's |
+            |       |
+            |       |
+            ---------
+
+
+
+                  *
+            */
+            let location = new LatLonPair(40.24982, -111.66243);
+            let res = await LocationService.getRestaurantAtLocation(location);
+            // console.log(res);
+            expect(res).toBeDefined();
+            expect(res?.name).toBe("McDonald's");
+        })
+
+
+        it("should locate Panda inside but near Pita", async ()=>{
+            /*
+            --------------------------
+            |       |                |
+            | Panda |      Pita      |
+            |     * |                |
+            |       |                |
+            --------------------------
+            */
+            let location = new LatLonPair(40.25107, -111.6583);
+            let res = await LocationService.getRestaurantAtLocation(location);
+            // console.log(res);
+            expect(res).toBeDefined();
+            expect(res?.name).toBe("Panda Express");
+        })
+
+      
+       
+        it("should locate Pit inside but near Panda", async ()=>{
+            /*
+            --------------------------
+            |       |                |
+            | Panda |      Pita      |
+            |       | *              |
+            |       |                |
+            --------------------------
+            */
+            let location = new LatLonPair(40.25107, -111.65826);
+            let res = await LocationService.getRestaurantAtLocation(location);
+            // console.log(res);
+            expect(res).toBeDefined();
+            expect(res?.name).toBe("Pita Pit");
+        })
+
+        
+        it("should locate Panda outside but near pita", async ()=>{
+            
+            /*
+            --------------------------
+            |       |                |
+            | Panda |      Pita      |
+            |       |                |
+            |       |                |
+            --------------------------
+                 *
+            */
+            let location = new LatLonPair(40.2510, -111.6583);
+            let res = await LocationService.getRestaurantAtLocation(location);
+            // console.log(res);
+            expect(res).toBeDefined();
+            expect(res?.name).toBe("Panda Express");
+        })
+
+        
+        // it.only("should locate Pita outside but near Panda", async ()=>{
+        
+        //     // THIS EDGE CASE FAILS!!!
+        //     // CASE: The user is not inside any known boundaries, is standing clearly
+        //     // outside the front of one restaurant but in a way that is closer to the
+        //     // geometric center of another.
+            
+        //     // Our algorithm does not determine the proximity to the walls of the
+        //     // restaurants, so it can't pickk out these cases.
+            
+        //     // Solutions:
+        //     //    1 - Minimize this case by only checking for restaurants when user
+        //     //        is not moving
+        //     //    2 - Reduce the query radius. This would simply the user need
+        //     //        to step closer to the building to the building to detect
+        //     //        either restaurant, but it wouldstill be possible for the
+        //     //        use case to occur. It would also limit our ability to catch
+        //     //        drive-thru visits.
+        //     //    3 - Change the algorithm. Possible, but unclear how at this point.
+        
+        
+        //     /*
+        //     ----------------------------
+        //     |       |                  |
+        //     | Panda |        Pita      |
+        //     |       |                  |
+        //     |       |                  |
+        //     ----------------------------
+        //               *
+        //     */
+        //     let location = new LatLonPair(40.2510, -111.658275);
+        //     let res = await LocationService.getRestaurantAtLocation(location);
+        //     console.log(res);
+        //     expect(res).toBeDefined();
+        //     expect(res?.name).toBe("Pita Pit");
+        // })
+
+
+        it("should locate Pita outside but in range of Panda", async ()=>{
+            /*
+            --------------------------
+            |       |                |
+            | Panda |      Pita      |
+            |       |                |
+            |       |                |
+            --------------------------
+                            *
+            */
+            let location = new LatLonPair(40.2510, -111.65815);
+            let res = await LocationService.getRestaurantAtLocation(location);
+            // console.log(res);
+            expect(res).toBeDefined();
+            expect(res?.name).toBe("Pita Pit");
+        })
+
     })
 })
--- backend/src/services/LocationService.ts
+++ backend/src/services/LocationService.ts
@@ -1,25 +1,73 @@
-import { LatLongPair } from "../../../react-native-app/shared/models/CoordinateModels";
+import { LatLonPair as LatLonPair } from "../../../react-native-app/shared/models/CoordinateModels";
 import RestaurantData from "../../../react-native-app/shared/models/RestaurantData";
 import OverpassAdapter from "../places_api/OverpassAdapter/OverpassAdapter";
-import PlacesApiAdapter from "../places_api/PlacesApiAdapter";
+import PlacesApiAdapter, { RestaurantDetectionDetails } from "../places_api/PlacesApiAdapter";
+const relationToPoly = require('robust-point-in-polygon')
 
 export class LocationService {
-    public static async getRestaurantAtLocation(coord: LatLongPair): Promise<RestaurantData | null> {
-        /**
-         * For the purposes of our demo, we will only check to see if
-         * the given location is within a small radius of the pin for
-         * the nearest restaurant.
-         * 
-         * Future algorithims may try to 
-         * determine that size/boundaries of the restaurant to be 
-         * more accurate.
-         */
-        const radius = 0.0002; // degree
+    /**
+     * Loads all restaurants within a small radius of the given coords and
+     * determines which one the coords are inside of or closest to.
+     */
+    public static async getRestaurantAtLocation(coord: LatLonPair): Promise<RestaurantData | null> {
+        /** Radius about the distance from McD's to their farthest drive-thru menu  */
+        const radius = 0.00015;
         let places = await this.getPlacesApiAdapter().getRestaurantsInRadius(coord,radius);
+        console.log(places)
+        
         if (places.length === 1) return places[0];
-        else return null;
+
+        if (places.length > 1) {
+            let closestPlace: RestaurantDetectionDetails|null = null;
+            let smallestDistance = Infinity;
+            for (let place of places) {
+                if (place.boundary) {
+                    // If the user is within the boundaries of this place,
+                    // return this place!
+                    if (this.isInBoundary(place.boundary, coord)) return place;
+                    // Otherewise, compute the pinLocation as the restaurant center
+                    // if it doesn't exist yet
+                    if (!place.pinLocation) {
+                        let lat = place.boundary.reduce((sum,p)=>sum+p.lat, 0)/place.boundary.length;
+                        let lon = place.boundary.reduce((sum,p)=>sum+p.lon, 0)/place.boundary.length;
+                        place.pinLocation = new LatLonPair(lat,lon)
+                    }
+                }
+
+                // If the plae has no boundaries, then the pinLocation is our only
+                // way of fixing it to a particular coordinate
+                if (place.pinLocation) {
+                    let dist = this.distanceBetween(coord, place.pinLocation)
+                    if (dist < smallestDistance) {
+                        closestPlace = place;
+                        smallestDistance = dist;
+                    }
+                }
+
+            }
+
+            if (closestPlace) return {
+                name: closestPlace.name,
+                location: closestPlace.pinLocation
+            }
+    
+        }
+
+        return null;
     }
 
+    public static distanceBetween(p1: LatLonPair, p2: LatLonPair): number {
+        let x1 = p1.lon
+        let x2 = p2.lon
+        let y1 = p1.lat
+        let y2 = p1.lat
+        return Math.sqrt( (x2-x1)**2 + (y2-y1)**2 )
+    }
+
+    public static isInBoundary(boundary: LatLonPair[], p: LatLonPair): boolean {
+        let poly = boundary.map(pt=>[pt.lat,pt.lon])
+        return relationToPoly(poly, [p.lat,p.lon]) === -1;
+    }
 
     private static getPlacesApiAdapter(): PlacesApiAdapter {
         // consider using a configureable factory for better flexibility
