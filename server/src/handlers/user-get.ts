6ac7cc5f1d5a95556c16034f4f98b49d101373b9
Arthur Judd <amjudd315@gmail.com>
2023-06-03 17:17:30 -0600

    log userid

--- server/src/handlers/user-get.ts
+++ server/src/handlers/user-get.ts
@@ -5,6 +5,7 @@ import UserService from "../services/UserService";
 export const handler = async (event: APIGatewayProxyEventV2) => {
     return await LambaUtils.handleEventWithBody<string>(event, async (userId,res)=>{
         let user = await new UserService().getUser(userId);
+		console.log(userId)
         if (user) {
             res.setBodyToData(user);
             res.setCode(200);
