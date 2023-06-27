2e4b06feb6067f435ee8c062776c8c4b9fdf3e47
Arthur Judd <amjudd315@gmail.com>
2023-06-27 06:27:06 -0600

    auth error handling

--- react-native-app/services/AuthService.ts
+++ react-native-app/services/AuthService.ts
@@ -40,27 +40,41 @@ const AuthService = {
 	},
 
 	async attemptLogin(email:string, password:string): Promise<User | undefined> {
-		return await auth()
-			.signInWithEmailAndPassword(email, password)
-			.then(async () => {
-				const input:any = auth().currentUser;
-				console.log('User account created & signed in!');
-				return await ServerFacade.getUserById(input.uid);
-			})
-			.catch(error => {
-				let errorMessage = '';
-				if (error.code === 'auth/user-not-found') {
-					errorMessage = "Email not recognized!";
-				}
-				if (error.code === 'auth/invalid-email') {
-					errorMessage = "Invalid email!";
-				}
-				if (error.code === 'auth/wrong-password') {
-					errorMessage = "Invalid password";
-				}
-				console.log(error.code)
-				throw new Error(errorMessage);
-			});
+		try {
+			await auth().signInWithEmailAndPassword(email, password)
+		}
+		catch (e: any) {
+			let errorMessage = 'Unknown error';
+			if (e.code === 'auth/user-not-found') {
+				errorMessage = "Email not recognized!";
+			}
+			if (e.code === 'auth/invalid-email') {
+				errorMessage = "Invalid email!";
+			}
+			if (e.code === 'auth/wrong-password') {
+				errorMessage = "Invalid password";
+			}
+			console.log(e, e.code)
+			throw new Error(errorMessage);
+		}
+		const userAuth:any = auth().currentUser;
+		console.log('User is authenticated, loading from DB');
+		
+		try {
+			console.log(userAuth)
+			let user = await ServerFacade.getUserById(userAuth.uid);
+			if (!user) {
+				// Assume this is the user's first log in after verification
+				// They must be added to db
+				// ServerFacade.createUser(new User(userAuth.uid, userAuth.email))
+				throw new Error("User is not verified");
+			}
+			return user;
+		}
+		catch (e) {
+			console.log(e);
+
+		}
 	},
 
 	async resendConfirmationEmail()  {
