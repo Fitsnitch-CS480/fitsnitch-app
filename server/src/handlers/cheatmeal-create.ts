0bdc35a6dcae0b2ba6cb60453d23079b4bbd8ebb
Arthur Judd <amjudd315@gmail.com>
2023-06-27 06:30:51 -0600

    cheatmeal compile

--- server/src/handlers/cheatmeal-create.ts
+++ server/src/handlers/cheatmeal-create.ts
@@ -2,9 +2,10 @@ import { APIGatewayProxyEventV2 } from "aws-lambda";
 import LambaUtils from "../utils/LambdaUtils";
 import CheatMealService from "../services/CheatMealService";
 import { CreateCheatMealRequest } from "../../../react-native-app/shared/models/requests/CreateCheatMealRequest";
+import CheatMealEvent from "../../../react-native-app/shared/models/CheatMealEvent";
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
-    return await LambaUtils.handleEventWithBody<CreateCheatMealRequest>(event, async (newCheatMealData, res)=>{
+    return await LambaUtils.handleEventWithBody<CheatMealEvent>(event, async (newCheatMealData, res)=>{
         try {
             await new CheatMealService().createCheatMeal(newCheatMealData);
             res.setBodyToData("Successfully created Cheat Meal!");
