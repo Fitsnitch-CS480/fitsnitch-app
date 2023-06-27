096507021f015fa0e20e2f5628d768558250aca6
Arthur Judd <amjudd315@gmail.com>
2023-06-27 06:26:51 -0600

    add safeview wrapper

--- react-native-app/App.tsx
+++ react-native-app/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { NativeInput } from './models/NativeInput';
 import AuthWrapper from './views/authWrapper';
 
@@ -6,6 +7,8 @@ export default function App(props){
   console.log("*****FIT INITIAL PROPS", props)
 
   return(
-	<AuthWrapper input={props as NativeInput} />
+	<SafeAreaView style={{backgroundColor: 'black', width: '100%', height: '100%'}}>
+		<AuthWrapper input={props as NativeInput} />
+	</SafeAreaView>
   )
 }
